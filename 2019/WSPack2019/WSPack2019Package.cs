1121c043c909545b695e192fa6556f97e24cc830
William Sade de Paiva <wilsade@msn.com>
2022-09-26 23:14:01 -0300

    inclusao do vsct

--- 2019/WSPack2019/WSPack2019Package.cs
+++ 2019/WSPack2019/WSPack2019Package.cs
@@ -27,6 +27,7 @@ namespace WSPack2019
   /// </remarks>
   [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
   [Guid(WSPack2019Package.PackageGuidString)]
+  [ProvideMenuResource("Menus.ctmenu", 1)]
   public sealed class WSPack2019Package : AsyncPackage
   {
     /// <summary>
@@ -48,6 +49,7 @@ namespace WSPack2019
       // When initialized asynchronously, the current thread may be a background thread at this point.
       // Do any initialization that requires the UI thread after switching to the UI thread.
       await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        await AboutCommand.InitializeAsync(this);
     }
 
     #endregion
