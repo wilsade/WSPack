42f728be3c286cbbc5f8583924f3a25d5fb0533b
William Sade de Paiva <wilsade@msn.com>
2022-09-28 22:41:51 -0300

    Builder para manutenção das solutions

---
+++ WSPack.Builder/BuilderObj.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSPack.Builder
+{
+  internal static class BuilderObj
+  {
+    const string MsBuildArguments = "{0} /p:Configuration=builder /m /clp:errorsOnly /noLogo";
+
+    static ProcessStartInfo GetProcessStartInfo(string solutionName) =>
+      new ProcessStartInfo("msbuild", string.Format(MsBuildArguments, solutionName))
+      {
+        UseShellExecute = false,
+        RedirectStandardOutput = true,
+        RedirectStandardError = true,
+        CreateNoWindow = true,
+      };
+
+    public static bool Build(IEnumerable<string> lst)
+    {
+      bool success = true;
+      foreach (var item in lst)
+      {
+        Console.WriteLine();
+        Console.WriteLine($"Compilando: {item}");
+        using (var p = Process.Start(GetProcessStartInfo(item)))
+        {
+          p.WaitForExit();
+          if (p.ExitCode != 0)
+          {
+            var output = p.StandardOutput.ReadToEnd();
+            var erros = p.StandardError.ReadToEnd();
+            success = false;
+            Console.WriteLine(output);
+            if (!string.IsNullOrWhiteSpace(erros))
+              Console.WriteLine(erros);
+          }
+          else
+          {
+            Console.WriteLine("  Sucesso");
+          }
+        }
+      }
+      return success;
+    }
+  }
+}
---
+++ WSPack.Builder/ProgramWSPackBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSPack.Builder
+{
+  internal class ProgramWSPackBuilder
+  {
+    enum ExitCodes
+    {
+      Sucess = 0,
+      NoSolutions = 1,
+      Error = 2
+    }
+
+    static int Main(string[] args)
+    {
+      IEnumerable<string> solutions = GetSolutions();
+      if (solutions == null)
+      {
+        Console.Write("Não foram encontradas solutions");
+        return (int)ExitCodes.NoSolutions;
+      }
+
+      Console.Clear();
+      if (!BuilderObj.Build(solutions))
+      {
+        Console.Beep();
+        Console.WriteLine();
+        Console.WriteLine("Erro(s) de build");
+        return (int)ExitCodes.Error;
+      }
+
+      return (int)ExitCodes.Sucess;
+    }
+
+    private static IEnumerable<string> GetSolutions()
+    {
+      var dirInfo = new DirectoryInfo(Path.GetDirectoryName(typeof(ProgramWSPackBuilder).Assembly.Location));
+      while (dirInfo?.Parent != null)
+      {
+        var lst = dirInfo.EnumerateFiles("WSPack*.sln");
+        if (lst.Any())
+          return lst.Select(x => x.FullName);
+        dirInfo = new DirectoryInfo(dirInfo.Parent.FullName);
+      }
+      return null;
+    }
+  }
+}
