f3c6681b28f59dc750e8d3dfef7e38d4887e5efd
William Sade de Paiva <wilsade@msn.com>
2022-09-29 22:13:39 -0300

    guidSymbols.vsct

---
+++ WSPack.VisualStudio.Shared/SharedAssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("pt-BR")]
+
+[assembly: AssemblyTitle("Wilsade - WSPack")]
+[assembly: AssemblyDescription("Pacote de funcionalidades para Visual Studio")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wilsade")]
+[assembly: AssemblyProduct("WSPack - 2019/2022")]
+[assembly: AssemblyCopyright("Copyright © William Sade de Paiva [desde 2022]")]
+[assembly: AssemblyTrademark("Wilsade")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
