6e51fd6e3481ca5c8939155dfb02b3319e36e6c0
William Sade de Paiva <wilsade@msn.com>
2022-10-06 23:05:49 -0300

    VSCTs OK

--- WSPack.Lib/Constantes.cs
+++ WSPack.Lib/Constantes.cs
@@ -15,7 +15,7 @@ namespace WSPack.Lib
     /// <summary>
     /// Nº de versão do Package e Assembly
     /// </summary>
-    public const string NumeroVersao = "4.0.0.2";
+    public const string NumeroVersao = "4.0.0.3";
 
     /// <summary>
     /// WSPack
