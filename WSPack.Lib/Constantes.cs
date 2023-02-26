11ee141ae1752429dfc5b283481afb68ca66ac91
William Sade de Paiva <wilsade@msn.com>
2023-02-26 10:55:17 -0300

    Mudando versao para 4.0.0.9

--- WSPack.Lib/Constantes.cs
+++ WSPack.Lib/Constantes.cs
@@ -15,7 +15,7 @@ namespace WSPack.Lib
     /// <summary>
     /// Nº de versão do Package e Assembly
     /// </summary>
-    public const string NumeroVersao = "4.0.0.8";
+    public const string NumeroVersao = "4.0.0.9";
 
     /// <summary>
     /// WSPack
