2f01e0e3149d53874410c54f879cee7357cccbf8
William Sade de Paiva <wilsade@msn.com>
2023-03-26 19:02:17 -0300

    ProjectsToolbarControl

--- WSPack.Lib/Properties/ResourcesLib.Designer.cs
+++ WSPack.Lib/Properties/ResourcesLib.Designer.cs
@@ -19,7 +19,7 @@ namespace WSPack.Lib.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class ResourcesLib {
@@ -126,6 +126,16 @@ namespace WSPack.Lib.Properties {
         public static System.Drawing.Bitmap imgPencil {
             get {
                 object obj = ResourceManager.GetObject("imgPencil", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap NewFile_16x {
+            get {
+                object obj = ResourceManager.GetObject("NewFile_16x", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
