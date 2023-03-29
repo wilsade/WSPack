036947a6e1fe12702fc4f6a002665a0a6145f2ec
William Sade de Paiva <wilsade@msn.com>
2023-03-29 20:40:11 -0300

    ProjectHorizontalScrollVisible

---
+++ WSPack.Lib.WPF/CommonLib/WSCheckBox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WSPack.Lib.WPF.CommonLib
+{
+  /// <summary>
+  /// CheckBox com estilo
+  /// </summary>
+  /// <seealso cref="CheckBox" />
+  public class WSCheckBox : CheckBox
+  {
+    /// <summary>
+    /// Cria uma instância da classe <see cref="WSCheckBox"/>
+    /// </summary>
+    public WSCheckBox()
+      : base()
+    {
+      if (WSPackFlexSupport.Instance?.VSStyler == null)
+        return;
+
+      SetResourceReference(ForegroundProperty, 
+        WSPackFlexSupport.Instance.VSStyler.ForegroundEx);
+    }
+  }
+}
