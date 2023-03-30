b2e9a18d4046cf86e782245b3b90f805414dffaa
William Sade de Paiva <wilsade@msn.com>
2023-03-29 21:03:01 -0300

    Build 4.0.0.17

--- WSPack.Lib/Constantes.cs
+++ WSPack.Lib/Constantes.cs
@@ -8,7 +8,7 @@
     /// <summary>
     /// Nº de versão do Package e Assembly
     /// </summary>
-    public const string NumeroVersao = "4.0.0.16";
+    public const string NumeroVersao = "4.0.0.17";
 
     /// <summary>
     /// WSPack
